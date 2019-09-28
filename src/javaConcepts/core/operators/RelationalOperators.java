ba851980a77c115f24d2e88c62ace98e98f39e41
Vipul Kumar Singh <vipul24.9669@gmail.com>
2019-09-28 23:29:46 +0530

    Relational Operators

---
+++ src/javaConcepts/core/operators/RelationalOperators.java
@@ -0,0 +1,17 @@
+package javaConcepts.core.operators;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 2 * 3;
+
+        System.out.println("a = " + a);
+        System.out.println("a == 6 : " + (a == 6));// Equal To
+        System.out.println("a != 5 : " + (a != 5));// Not equal to
+        System.out.println("a > 7 : " + (a > 7));// Greater than
+        System.out.println("a < 6 : " + (a < 6));// Less than
+        System.out.println("a >= 5 : " + (a >= 5));// Greater than or equal to
+        System.out.println("a <= 6 : " + (a <= 6));// Less than or equal to
+    }
+}
