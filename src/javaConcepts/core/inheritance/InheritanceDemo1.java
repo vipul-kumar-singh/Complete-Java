d735d050c05595c780703e01600644f4c8c82b32
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-03-01 23:45:16 +0530

    Inheritance Demo 1

--- src/javaConcepts/core/inheritance/InheritanceDemo1.java
+++ src/javaConcepts/core/inheritance/InheritanceDemo1.java
@@ -1,4 +1,48 @@
-package javaConcepts.core.inheritence;
+package javaConcepts.core.inheritance;
 
-public class Inheritance {
+public class InheritanceDemo1 {
+
+    public static void main(String[] args) {
+        Parent parent1 = new Parent();
+        System.out.println(parent1.getA());
+
+        Parent parent2 = new Child();
+        System.out.println(parent2.getA());
+
+        Child child1 = new Child();
+        System.out.println(child1.getA());
+
+        Parent parent3 = child1;
+        System.out.println(parent3.getA());
+
+        child1.setA(10);
+        System.out.println(((Parent)child1).getA());
+        System.out.println(child1.getParentA());
+    }
+}
+
+class Parent {
+    private int a = 5;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
+
+
+class Child extends Parent{
+
+    private int a = 4;
+
+    public int getA(){
+        return a;
+    }
+
+    public int getParentA(){
+        return super.getA();
+    }
 }
